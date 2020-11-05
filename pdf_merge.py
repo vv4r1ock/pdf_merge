e1d3a6007843859d5cc5991c40a0f04f16947948
0x7ea8 <73687876+0x7ea8@users.noreply.github.com>
2020-11-05 16:21:33 +0800

    Create pdf_merge.py

---
+++ pdf_merge.py
@@ -0,0 +1,25 @@
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # 把每张PDF页面加入到这个可读取对象中
+            pdf_writer.addPage(pdf_reader.getPage(page))
+    # 把这个已合并了的PDF文档存储起来
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+paths = os.listdir()
+for i in paths:
+    if i.split('.')[-1] != 'pdf' :
+        paths.remove(i)
+        print("发现非pdf文件:{}".format(i))
+
+#print("开始合并pdf文件:{}".format(paths))
+#"合并后"这个名字自己改吧
+merge_pdfs(paths, output='合并后.pdf')
+#print("合并完成")
